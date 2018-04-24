53554a7ec83c4aac0c8770a31d2abbdcd26a3812
Fausto Biazzi de Sousa <faustobiazzi@gmail.com>
2018-04-23 22:06:48 -0300

    Add files via upload

--- Redimensionador.py
+++ Redimensionador.py
@@ -1,25 +1,42 @@
-import sys
-import os
+import os , glob
 try:
     from PIL import Image
 except ImportError:
     import Image
 
-
-
-files_dir = "."
-print (files_dir)
-arquivo = [f for f in os.listdir(files_dir) if f.endswith("JPG")]
+    
+countfile = 0
+countfolder = 0
 largura_desejada = 800
-        
-for filename in arquivo:
-    print("trabalhando imagem "+filename)
-    imagem = Image.open(files_dir+"/"+filename)
-    largura_imagem = imagem.size[0]
-    altura_imagem = imagem.size[1]
-    percentual_largura = float(largura_desejada) / float(largura_imagem)
-    altura_desejada = int((altura_imagem * percentual_largura))
 
-    imagem = imagem.resize((largura_desejada, altura_desejada), Image.ANTIALIAS)
-    imagem.save(filename)
-   
+with open("error.txt", "w") as a:
+    for path, subdirs, files in os.walk(r'.'):
+       countfolder = countfolder+1
+       for filename in files:
+             if(".jpg" in filename) or (".JPG" in filename) or (".jpeg" in filename) or (".JPEG" in filename):
+                countfile = countfile +1
+            
+                f = os.path.join(path, filename)
+                print("trabalhando imagem "+f)
+                imagem = Image.open(f)
+                largura_imagem = imagem.size[0]
+                if (largura_imagem > largura_desejada):
+                    try:
+                        altura_imagem = imagem.size[1]
+                        percentual_largura = float(largura_desejada) / float(largura_imagem)
+                        altura_desejada = int((altura_imagem * percentual_largura))
+                        imagem = imagem.resize((largura_desejada, altura_desejada), Image.ANTIALIAS)
+                        imagem.save(f)
+                        #print ("imagem " + filename + " redimensionada)
+                    except Exception as ERROR:
+                        a.write(str(f) + os.linesep)
+                        a.write(str(ERROR) + os.linesep)
+                        continue
+                else:
+                    continue
+print ("Foram encontrados "+ str(countfile) + " Arquivos" ) 
+print ("Foram encontrados "+ str(countfolder) + " Pastas" )
+os.system("pause")
+input ("pressione qualquer tecla para terminar")
+exit()
+
